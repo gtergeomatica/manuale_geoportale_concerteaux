ecc83fd6d09e0e841f5c1cdcdc480ae21678c118
rmarzocchi84 <roberto.marzocchi@gter.it>
2020-03-18 11:11:55 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -326,3 +326,5 @@ intersphinx_mapping = {'https://docs.python.org/': None}
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
+
+gettext_compact = True
